29f23a327b03a0fc674b061261299d8163b387e4
tgaff <tgaff@alumni.nd.edu>
2016-03-07 22:52:02 -0800

    create works in react

--- react.jsx
+++ react.jsx
@@ -14,6 +14,13 @@ var BooksContainer = React.createClass({
     }.bind(this));
   },
 
+  // BooksContainer owns the books data
+  // in order to let child components alter that data we need to pass
+  //   functions down to them from here for them to call.
+  addNewItem: function(book) {
+    this.setState({books: this.state.books.concat(book) } );
+  },
+
   render: function() {
     var rows = [];
     console.log('hi', this.state.books);
@@ -25,6 +32,7 @@ var BooksContainer = React.createClass({
 
     return (
       <div id="books-list">
+        <BookForm addNewItem={this.addNewItem} />
         {rows}
       </div>
     )
@@ -89,6 +97,48 @@ var Book = React.createClass({
     );
   }
 });
+
+
+/*  Form for creating a new book */
+var BookForm = React.createClass({
+  render: function() {
+    return (
+      <form id="create-book" onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <input type="text" name="title" className="form-control" placeholder="Title"  autofocus ref="title" />
+        </div>
+        <div className="form-group">
+          <input type="text" name="author" className="form-control" placeholder="Author" ref="author" />
+        </div>
+        <div className="form-group">
+          <input type="text" name="image" className="form-control" placeholder="Image" ref="image" />
+        </div>
+        <div className="form-group">
+          <input type="text" name="releaseDate" className="form-control" placeholder="Release Date" ref="releaseDate" />
+        </div>
+        <div className="form-group">
+          <input type="submit" className="btn btn-block btn-primary" value="Save" />
+        </div>
+      </form>
+    )
+  },
+
+  handleSubmit: function(e) {
+    var vm = this;
+    e.preventDefault();
+    console.log('you submitted', this.refs.title.value, this.refs.author.value, this.refs.image.value, this.refs.releaseDate.value);
+    var newBook = { title: this.refs.title.value,
+                    author: this.refs.author.value,
+                    image: this.refs.image.value,
+                    releaseDate: this.refs.releaseDate.value
+                  };
+    $.post(baseUrl, newBook).success(function(result) {
+      console.log('got response', result);
+      // the books list isn't controlled here, we have to call a passed down function to update the parent component
+      vm.props.addNewItem(result);
+    });
+  }
+})
 
 
 React.render(
